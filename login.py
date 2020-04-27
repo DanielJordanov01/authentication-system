600b5bab4da734761be6d126f9c09bab328b5707
Даниел Валентинов <daniel159696@gmail.com>
2020-04-27 11:49:44 +0300

    created log in feature

---
+++ login.py
@@ -0,0 +1,19 @@
+import sys
+import bcrypt
+from packages.database.db_setup import db
+
+email = sys.argv[1]
+password = sys.argv[2]
+
+users = db.users
+
+password = password.encode()
+
+def check_credentials(user):
+	if(user['email'] == email and bcrypt.checkpw(password, user['password'])):
+		print('Loged in!')
+	else:
+		print('Wrong credentials!')
+
+for user in users.find():
+	check_credentials(user)
--- register.py
+++ register.py
@@ -1,4 +1,5 @@
 import sys
+import bcrypt
 from packages.database.db_setup import db
 from packages.check_uniqueness.check_uniqueness import is_user_valid
 
@@ -9,17 +10,24 @@ password = sys.argv[3]
 
 users = db.users
 
+# convert password to bytes
+password = password.encode()
+
 # Recieves True or Flase if the user data is valid
 is_valid = is_user_valid(email, name, password)
 
+# hash password
+hashed_pw = bcrypt.hashpw(password, bcrypt.gensalt())
+
+# register user
 if(is_valid):
 	user_data = {
 		'name': name,
 		'email': email,
-		'password': password
+		'password': hashed_pw
 	}
 
 	user = users.insert_one(user_data)
 	print('Registered!')
 else:
-	print('error')
+	print('Error')
